e8c7653faaf258809d20ab3ed42c6da44bf44cf7
abdulrahman <abdulrahmanfarhan1985@gmail.com>
2025-03-03 16:28:28 +0300

    Updated project files

--- src/main/java/rectangles/Rectangle.java
+++ src/main/java/rectangles/Rectangle.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rectangles;
 
 /**
  *
- * @author Abdulrahman
+ * @author Abdulrahman_Farhan
  */
 public class Rectangle {
-    
+
+    private double height = 1;
+    private double width = 1;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return (height * width);
+    }
+
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+    public double getWidth(){
+        return width;
+    } 
+    public double getHeight(){
+        return height;
+    } 
 }
--- src/main/java/rectangles/TestRectangle.java
+++ src/main/java/rectangles/TestRectangle.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package rectangles;
 
 /**
@@ -9,5 +5,19 @@ package rectangles;
  * @author Abdulrahman
  */
 public class TestRectangle {
-    
+
+    public static void main(String[] args) {
+        rectangles.Rectangle r1 = new Rectangle(4, 40);
+        rectangles.Rectangle r2 = new Rectangle(3.5, 35.9);
+        
+        System.out.println("The width of rectangle r1 is " + r1.getWidth());
+        System.out.println("The height of rectangle r1 is " + r1.getHeight());
+        System.out.println("The area of rectangle r1 is: " + r1.getArea());
+        System.out.println("The perimeter of rectangle r1 is: " + r1.getPerimeter());
+        System.out.println("--------------------------------");
+        System.out.println("The width of rectangle r2 is " + r2.getWidth());
+        System.out.println("The height of rectangle r2 is " + r2.getHeight());
+        System.out.println("The area of rectangle r2 is: " + r2.getArea());
+        System.out.println("The perimeter of rectangle r2 is: " + r2.getPerimeter());
+    }
 }
