88b1b64644e85fb3ef72c90d5eaf0cda174a3df5
abdulrahman <abdulrahmanfarhan1985@gmail.com>
2025-03-15 20:10:52 +0300

    Updated project files

---
+++ src/main/java/extraProblemsAndCode/RandomNumberGenerator.java
@@ -0,0 +1,24 @@
+package extraProblemsAndCode;
+
+/*
+(Use the Random class)
+Write a program that creates 
+a Random object with seed 1000
+and displays the first 50 random integers 
+between 0 and 100 using the nextInt(100) method.
+ */
+import java.util.Random;
+
+public class RandomNumberGenerator {
+
+    public static void main(String[] args) {
+        Random r = new Random(1000);
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(r.nextInt(100) + " ");
+            if (i % 10 == 0) {
+                System.out.println("");
+            }
+        }
+    }
+}
