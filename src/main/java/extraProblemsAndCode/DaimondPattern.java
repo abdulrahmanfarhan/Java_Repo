b84b9850e5cbf0157907d59d72dc3f4900c68088
abdulrahman <abdulrahmanfarhan1985@gmail.com>
2025-03-18 01:19:30 +0300

    Updated project files

--- src/main/java/extraProblemsAndCode/DaimondPattern.java
+++ src/main/java/extraProblemsAndCode/DaimondPattern.java
@@ -1,5 +1,7 @@
 package extraProblemsAndCode;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Abdulrahman
@@ -7,38 +9,136 @@ package extraProblemsAndCode;
 public class DaimondPattern {
 
     public static void main(String[] args) {
-        int n = 5;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = s.nextInt();
         for (int i = 1; i < n; i++) {
+            //0000000000000000000000000000000000000000000(1/1)
+            System.out.print("|");
             for (int j = 0; j <= ((n - i)); j++) {
                 System.out.print("/");
             }
             for (int K = 0; K < (i * 2) - 1; K++) {
-                if (K == 0 || K == ((i * 2) - 1) - 1) {
+                if (K == 0 || K == (i * 2) - 2) {
+                    //frame
                     System.out.print(" ");
                 } else {
-                    System.out.print("*");
+                    if (K % 2 == 0) {
+                        System.out.print(".");
+                    } else {
+                        System.out.print(" ");
+                    }
                 }
             }
             for (int j = 0; j <= ((n - i)); j++) {
                 System.out.print("\\");
             }
+            //1111111111111111111111111111111111111111111111(1/2)
+            System.out.print("|");
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("/");
+            }
+            for (int K = 0; K < (i * 2) - 1; K++) {
+                if (K == 0 || K == (i * 2) - 2) {
+                    //frame
+                    System.out.print(" ");
+                } else {
+                    if (K % 2 == 0) {
+                        System.out.print("^");
+                    } else {
+                        System.out.print("o");
+                    }
+                }
+            }
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("\\");
+            }
+            //0000000000000000000000000000000000000000000(2/1)
+            System.out.print("|");
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("/");
+            }
+            for (int K = 0; K < (i * 2) - 1; K++) {
+                if (K == 0 || K == (i * 2) - 2) {
+                    //frame
+                    System.out.print(" ");
+                } else {
+                    if (K % 2 == 0) {
+                        System.out.print(".");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("\\");
+            }
+            System.out.print("|");
             System.out.println("");
         }
 
         for (int i = n; i >= 1; i--) {
+            //0000000000000000000000000000000000000000000(2/2)
+            System.out.print("|");
             for (int j = 0; j <= ((n - i)); j++) {
                 System.out.print("\\");
             }
             for (int K = 0; K < (i * 2) - 1; K++) {
-                if (K == 0 || K == ((i * 2) - 1) - 1) {
+                if (K == 0 || K == (i * 2) - 2) {
+                    //frame
                     System.out.print(" ");
                 } else {
-                    System.out.print("*");
+                    if (K % 2 == 0) {
+                        System.out.print("v");
+                    } else {
+                        System.out.print("*");
+                    }
                 }
             }
             for (int j = 0; j <= ((n - i)); j++) {
                 System.out.print("/");
             }
+            //111111111111111111111111111111111111111(3/1)
+            System.out.print("|");
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("\\");
+            }
+            for (int K = 0; K < (i * 2) - 1; K++) {
+                if (K == 0 || K == (i * 2) - 2) {
+                    //frame
+                    System.out.print(" ");
+                } else {
+                    if (K % 2 == 0) {
+                        System.out.print(".");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("/");
+            }
+            //0000000000000000000000000000000000000000000(3/2)
+            System.out.print("|");
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("\\");
+            }
+            for (int K = 0; K < (i * 2) - 1; K++) {
+                if (K == 0 || K == (i * 2) - 2) {
+                    //frame
+                    System.out.print(" ");
+                } else {
+                    if (K % 2 == 0) {
+                        System.out.print("v");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+            }
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print("/");
+            }
+            System.out.print("|");
             System.out.println("");
         }
     }
