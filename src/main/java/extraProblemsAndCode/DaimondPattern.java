5e93a8b22719a2e94a71e91df284352a2fc20dc1
abdulrahman <abdulrahmanfarhan1985@gmail.com>
2025-03-17 23:33:17 +0300

    Updated project files

---
+++ src/main/java/extraProblemsAndCode/DaimondPattern.java
@@ -0,0 +1,31 @@
+package extraProblemsAndCode;
+
+/**
+ *
+ * @author Abdulrahman
+ */
+public class DaimondPattern {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print(" ");
+            }
+            for (int K = 0; K < (i * 2) - 1; K++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j <= ((n - i)); j++) {
+                System.out.print(" ");
+            }
+            for (int K = 0; K < (i * 2) - 1; K++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ src/main/java/extraProblemsAndCode/TrianglePattern.java
@@ -0,0 +1,25 @@
+package extraProblemsAndCode;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Abdulrahman
+ */
+public class TrianglePattern {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = s.nextInt();
+        String star = "";
+        for (int i = 1; i <= n; i++) {
+            star += "*";
+            System.out.println(star);
+        }
+        System.out.println("-------------------");
+        for (int i = n; i >= 1; i--) {
+            System.out.println("*".repeat(i));
+        }
+    }
+}
