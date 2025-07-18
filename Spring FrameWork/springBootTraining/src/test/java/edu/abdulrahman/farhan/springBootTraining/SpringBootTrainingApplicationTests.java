8feab474c708ffaae6024c0a7429cc74fa009e46
abdulrahmanfarhan <abdulrahmanfarhan1985@gmail.com>
2025-07-18 17:13:17 +0300

    Updating Examples

---
+++ Spring FrameWork/springBootTraining/src/test/java/edu/abdulrahman/farhan/springBootTraining/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.abdulrahman.farhan.springBootTraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
